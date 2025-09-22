175fd117cb7a42ea02e21dee1a9451a42d29a0d2
hugovanolmen <hugo.vanolmen@gruposantander.com>
2025-09-22 12:05:57 +0200

    cherry pick completed

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,12 +2,8 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("new feature");
-
-        int i=0;
-        System.out.println(i);
-        i++;
-        System.out.println(i);
+        System.out.println("Hello from the second new branch!");
+        System.out.println("Hello from the first new branch!");
+        System.out.println("Merge resolved");
     }
 }
