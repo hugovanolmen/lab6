474f49b506d18db70b4e8979ec7d1bd1a8000393
hugovanolmen <hugo.vanolmen@gruposantander.com>
2025-09-22 12:52:36 +0200

    changes2

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("new feature");
+
+        int i=0;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
     }
 }
