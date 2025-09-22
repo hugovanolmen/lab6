cebdc8ee37a3a5f3f56686873bceeedec650a107
hugovanolmen <hugo.vanolmen@gruposantander.com>
2025-09-22 11:01:27 +0200

    merge resolved

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,10 +2,8 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello from the second new branch!");
-=======
         System.out.println("Hello from the first new branch!");
->>>>>>> branch1
+        System.out.println("Merge resolved");
     }
 }
