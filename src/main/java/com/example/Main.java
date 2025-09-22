fc845bc510a06288fa02edaf6460a223c7e78a60
hugovanolmen <hugo.vanolmen@gruposantander.com>
2025-09-22 10:58:28 +0200

    branch2 first commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Cambios feature-branch");
+        System.out.println("Hello from the second new branch!");
     }
 }
